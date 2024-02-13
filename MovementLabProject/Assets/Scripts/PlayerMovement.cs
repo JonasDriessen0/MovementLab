762501dab301045085af293a6e7d6917a1454217
JonasDriessen0 <114576261+JonasDriessen0@users.noreply.github.com>
2024-02-14 00:00:45 +0100

    Imported Unity Project

---
+++ MovementLabProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Basic properties")]
+    public float speed;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    private Vector3 velocity;
+
+    [Header("Ground properties")]
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    [Header("Refrences")]
+    public Animator animator;
+    public CharacterController controller;
+    public Camera cam;
+
+    void Update()
+    {
+        //See if the player is grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        //Stop downwards velocity from increasing while on the ground
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            animator.SetTrigger("Jump");
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        float moveInputMagnitude = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).sqrMagnitude;
+    }
+}
